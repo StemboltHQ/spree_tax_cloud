fc6b2d1111a4809be28a09448f5ff20fd43c050a
Brian Buchalter <bal711@gmail.com>
2014-09-10 11:42:02 -0600

    Do not rescue checkout tax cloud errors

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,13 +1,15 @@
-Spree::CheckoutController.class_eval do
+# We handle our own tax cloud errors
 
-  rescue_from SpreeTaxCloud::Error do |exception|
-    flash[:error] = exception.message
-    redirect_to checkout_state_path(:address)
-  end
-
-  rescue_from TaxCloud::Errors::ApiError do |exception|
-    flash[:error] = Spree.t("address_verification_failed")
-    redirect_to checkout_state_path(:address)
-  end
-
-end
+# Spree::CheckoutController.class_eval do
+#
+#   rescue_from SpreeTaxCloud::Error do |exception|
+#     flash[:error] = exception.message
+#     redirect_to checkout_state_path(:address)
+#   end
+#
+#   rescue_from TaxCloud::Errors::ApiError do |exception|
+#     flash[:error] = Spree.t("address_verification_failed")
+#     redirect_to checkout_state_path(:address)
+#   end
+#
+# end
